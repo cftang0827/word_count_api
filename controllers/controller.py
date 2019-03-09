913e32dc03951cbe86c56ab997d8ab1da2246167
cftang_home <cftang0827@gmail.com>
2019-03-10 04:43:15 +0800

    add first version of controller and word count method

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,1 +1,11 @@
 from models import model
+import re
+import requests
+
+
+class WordCounter():
+    def count(url, keyword):
+        r = requests.get(url)
+        html_str = str(r.content, encoding='utf-8')
+        result = re.findall('(^|\s)(' + keyword + ')($|\s)', html_str)
+        return len(result)
