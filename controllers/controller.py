b26312dc320479b0a15a107adf09e33bb01c5c1c
cftang_home <cftang0827@gmail.com>
2019-03-10 05:13:22 +0800

    add word count string unittest

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,8 +4,11 @@ import requests
 
 
 class WordCounter():
-    def count(url, keyword):
+    def count_from_url(self, url, keyword):
         r = requests.get(url)
         html_str = str(r.content, encoding='utf-8')
-        result = re.findall('(^|\s)(' + keyword + ')($|\s)', html_str)
+        return self.count_from_string(html_str, keyword)
+
+    def count_from_string(self, string, keyword):
+        result = re.findall('(^|\s)(' + keyword + ')($|\s)', string)
         return len(result)
---
+++ tests/test_word_count.py
@@ -0,0 +1,28 @@
+import unittest
+from controllers.controller import WordCounter
+
+
+class TestWordCountString(unittest.TestCase):
+    def setUp(self):
+        self.word_counter = WordCounter()
+
+    def tearDown(self):
+        self.word_counter = None
+
+    def test_count_from_short_string(self):
+        expected = 4
+        test = "fit is good fit fitting, and fits is afit a fit, and fit is good, fit"
+        result = self.word_counter.count_from_string(test, "fit")
+        self.assertEqual(expected, result,
+                         "Error, the counting number is not correct !")
+
+    def test_count_from_long_string(self):
+        expected = 16
+        test = "fit is good fit fitting, and fits is afit a fit, and fit is good, fit fit is good fit fitting, and fits is afit a fit, and fit is good, fit fit is good fit fitting, and fits is afit a fit, and fit is good, fit fit is good fit fitting, and fits is afit a fit, and fit is good, fit"
+        result = self.word_counter.count_from_string(test, "fit")
+        self.assertEqual(expected, result,
+                         "Error, the counting number is not correct !")
+
+
+if __name__ == '__main__':
+    unittest.main()
