1fcd22d83b71a83252e32e95c44978da6a83c4c5
cftang <cftang@cftangde-MacBook-Pro-2.local>
2019-03-11 16:26:45 +0800

    add app.py error handler for every http error code and 500 server error code, add logger

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
 import logging
 from datetime import datetime
+
+from flask import Flask, jsonify, request
 
 from controllers import controller
 
@@ -8,51 +9,47 @@ app = Flask(__name__)
 
 word_counter = controller.WordCounter()
 
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-            'status': 404,
-            'message': 'Not Found: ' + request.url,
-    }
-    app.logger.error("Page Not Found: {}".format(request.url))
-    resp = jsonify(message)
-    resp.status_code = 404
-
-    return resp
-
 
 @app.errorhandler(Exception)
-def internal_error(e):
+def unhandle_error(e):
     message = {
-        'status': 500,
-        'message': 'Internal Server Error, Error Message: {}'.format(str(e))
+        'message': 'Error Message: {}'.format(str(e)),
     }
-    app.logger.error("Internal Server Error, Error Message: {}".format(str(e)))
+    status_str = str(e).split(' ')[0]
+    app.logger.error("Error Message: {}".format(str(e)))
+
+    if status_str.isdigit():
+        status_code = int(status_str)
+    else:
+        status_code = 500
+    message.update({"status": status_code})
+
     resp = jsonify(message)
-    resp.status_code = 500
+    resp.status_code = status_code
 
     return resp
+
 
 @app.route('/wordcount', methods=['POST'])
 def wordcount():
-        if request.method == 'POST':
-            word = request.form['word']
-            url = request.form['url']
-            result = word_counter.count_from_url(url, word)
-            result.update({
-                "status": "ok"
-            })
-            resp = jsonify(result)
-        else:
-            raise Exception("Not A Valid Post Request")
-        
+    if request.method == 'POST':
+        word = request.form['word']
+        url = request.form['url']
+        result = word_counter.count_from_url(url, word)
+        result.update({"status": "ok"})
+        app.logger.info("Response ok")
+        resp = jsonify(result)
+
         return resp
 
+
 if __name__ == '__main__':
-    handler = logging.FileHandler('./log/log_{}.log'.format(datetime.utcnow().strftime('%b_%d_%y_%H_%M_%S')))
-    handler.setLevel(logging.DEBUG)
+    handler = logging.FileHandler('./log/log_{}.log'.format(
+        datetime.utcnow().strftime('%b_%d_%y_%H_%M_%S')))
     logging_format = logging.Formatter(
-        '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+        '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s'
+    )
     handler.setFormatter(logging_format)
+    app.logger.setLevel(logging.INFO)
     app.logger.addHandler(handler)
     app.run(port=8000)
